74377ccb7dd4a2140a8e7d09350864215bbd0601
Ali170800 <alidiouf170800@gmail.com>
2026-06-16 18:10:27 +0000

    Update EmailUtil.java

--- src/main/java/com/amical/ard/utils/EmailUtil.java
+++ src/main/java/com/amical/ard/utils/EmailUtil.java
@@ -14,10 +14,15 @@ public class EmailUtil {
     public static void envoyerEmail(String destinataire, String sujet, String corps) {
 
         Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.auth", "true");
+
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.fallback", "false");
+
         props.put("mail.smtp.connectiontimeout", "10000");
         props.put("mail.smtp.timeout", "10000");
 
