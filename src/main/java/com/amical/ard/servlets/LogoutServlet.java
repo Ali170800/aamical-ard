4e44b3c99fec3a900317fa6f8127ba68dd9719ba
Ali170800 <alidiouf170800@gmail.com>
2026-06-16 17:11:36 +0000

    Update LogoutServlet.java

--- src/main/java/com/amical/ard/servlets/LogoutServlet.java
+++ src/main/java/com/amical/ard/servlets/LogoutServlet.java
@@ -8,19 +8,20 @@ import java.io.IOException;
 
 @WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
             throws ServletException, IOException {
 
         HttpSession session = request.getSession(false);
+
         if (session != null) {
             session.invalidate();
         }
 
-<<<<<<< HEAD
-        response.sendRedirect(request.getContextPath() + "/login.jsp");
-=======
-        response.sendRedirect(request.getContextPath() + "/accueil.jsp");
->>>>>>> cc8371319608e422e08ccbcba184f02a94198195
+        response.sendRedirect(
+                request.getContextPath() + "/accueil.jsp"
+        );
     }
 }
